122d57dcdbcfc05d832fcbe4e3768850c4201ded
42494 <4249495@gmail.com>
2022-02-12 01:30:59 +0300

    demo1

---
+++ Sber/Assets/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public Animator gameOverAnim;
+    public AudioClip overAudio;
+    AudioSource audioS;
+
+    // Update is called once per frame
+    void Start()
+    {
+        gameOverAnim = gameOverAnim.GetComponent<Animator>();
+        gameOverAnim.enabled = false;
+        audioS = GetComponent<AudioSource>();
+    }
+    public void GameOverFun()
+    {
+        audioS.clip = overAudio;
+        if (!audioS.isPlaying)
+        {
+            audioS.Play();
+        }
+        audioS.volume = 1;
+        gameOverAnim.enabled = true;
+        gameOverAnim.SetBool("gameOver", false);
+    }
+    public void beginClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Sber/Assets/Master.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Master : MonoBehaviour
+{
+    public Animator panelAnim;
+    public Animator infoAnim;
+    void Start()
+    {
+        panelAnim = panelAnim.GetComponent<Animator>();
+        infoAnim = infoAnim.GetComponent<Animator>();
+        panelAnim.enabled = false;
+        infoAnim.enabled = false;
+    }
+
+    // Update is called once per frame
+    public void SettingsClick()
+    {
+        panelAnim.enabled = true;
+        panelAnim.SetBool("out", false);
+    }
+    public void SettingsClose()
+    {
+        panelAnim.SetBool("out", true);
+    }
+    public void InfoClick()
+    {
+        infoAnim.enabled = true;
+        infoAnim.SetBool("outInfo", false);
+    }
+    public void InfoClose()
+    {
+        infoAnim.SetBool("outInfo", true);
+    }
+    public void StartClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
